938dcf88ab876443093393ded2d236a705449605
AyushKayastha <ayushkayastha101@gmail.com>
2024-06-11 09:52:09 +0545

    version-6 Ayush version-6

--- lib/features/widget/hotel_profile.dart
+++ lib/features/widget/hotel_profile.dart
@@ -14,6 +14,8 @@ class _HotelProfileState extends State<HotelProfile> {
 
   @override
   Widget build(BuildContext context) {
+    Color mycolor =Color.fromARGB(255, 81, 212, 194);
+
     return Scaffold(
       appBar: AppBar(title: Expanded(
           child: Text(hotelDetails.name[widget.index],
@@ -24,6 +26,69 @@ class _HotelProfileState extends State<HotelProfile> {
       ),
       ),
 
+
+    body: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(32.0),
+            child: Image.network(
+              'https://via.placeholder.com/400', // Dummy image URL
+              height: 200.0,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Deluxe Room',
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  '\$220 /per night',
+                  style: TextStyle(fontSize: 20.0, color: Colors.grey[700]),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Sleeps 3 people',
+                  style: TextStyle(fontSize: 16.0, color: Colors.grey[600]),
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Book now',
+                        style: TextStyle(
+                            color: Colors.black
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: mycolor,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text('Room details'),
+                      style: TextButton.styleFrom(
+                        foregroundColor: Colors.grey[700], // Text color
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    ),
     );
   }
 }
